eb6d25ada88cb5f38c6a5bc38a6e1b8b3bddfd8f
Kaspars Dambis <kaspars@konstruktors.com>
2013-08-29 11:57:26 +0300

    Add stats too

--- plugin.php
+++ plugin.php
@@ -1,9 +1,61 @@
 <?php
 /*
-Plugin Name: Yearly Archive Shortcode
-Description: Provides a <code>[yearly_archive]</code> shortcode
+Plugin Name: Yearly Archive and Stats Shortcode
+Description: Provides <code>[yearly_archive]</code> and [yearly_archive_stats] shortcodes
+Version: 1.2
 Author: Kaspars Dambis
 */
+
+
+add_shortcode( 'yearly_archive_stats', 'yas_stats' );
+
+function yas_stats() {
+
+	$maybe_cache = get_transient( 'yearly_archive_stats_render' );
+
+	if ( ! empty( $maybe_cache ) )
+		return $maybe_cache;
+
+	// Count totals
+	$post_count = wp_count_posts();
+	$comment_count = wp_count_comments();
+	$first_post = get_posts( array( 'posts_per_page' => 1, 'order' => 'ASC' ) );
+	$first_post_date = strtotime( $first_post[0]->post_date );
+	$time_between_posts = ( time() - strtotime( $first_post[0]->post_date ) ) / $post_count->publish;
+
+	// Count words
+	$all_posts = get_posts( array( 'posts_per_page' => -1 ) );
+	$word_count = array();
+	$week_stats = array();
+	foreach ( $all_posts as $post ) {
+		$week_stats[ date( 'l', strtotime( $post->post_date ) ) ] += 1;
+		$word_count[] = str_word_count( strip_tags( $post->post_content ) );
+	}
+	arsort( $week_stats );
+	$week_stats = array_keys( $week_stats );
+	$word_count_total = array_sum( $word_count );
+
+	$stats_render = sprintf(
+		'In %s since %s I have written %d blog posts mostly on %s and %s (one post per %s on average) which have received %d comments.
+		Posts are composed of %d words in total with %s words per post on average which would be a book of %d pages.',
+		human_time_diff( $first_post_date ),
+		date( 'F n, Y', $first_post_date ),
+		$post_count->publish,
+		$week_stats[0] . 's',
+		$week_stats[1] . 's',
+		human_time_diff( time() - $time_between_posts ),
+		$comment_count->approved,
+		$word_count_total,
+		intval( $word_count_total / $post_count->publish ),
+		intval( $word_count_total / 250 ) 
+	);
+
+	set_transient( 'yearly_archive_stats_render', $stats_render, 24*60*60 );
+
+	return $stats_render;
+
+}
+
 
 add_shortcode( 'yearly_archive', 'yas_shortcode' );
 
